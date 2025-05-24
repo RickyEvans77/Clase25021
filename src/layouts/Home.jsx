3fba68f8e87f2edbb550e43b79e7ffafde5facf6
RickyEvans77 <rickynerd@hotmail.com>
2025-05-24 10:38:21 -0300

    proyecto inicial

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,17 @@
+import Nav from "../componentes/Nav.jsx"
+import Header from "../componentes/Header.jsx"
+import Main from "../componentes/Main.jsx"
+import Footer from "../componentes/Footer"
+
+function Home(){
+
+    return(
+        <div>
+            <Nav/>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+export default Home
