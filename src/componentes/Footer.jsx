52eefa5f6ded9d9bcce5053182848581427cf4bf
RickyEvans77 <rickynerd@hotmail.com>
2025-06-09 08:13:22 -0300

    Arregado productos detalle.css

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -6,16 +6,19 @@ function Footer() {
         <footer style={{
             backgroundColor: "#f1f1f1", padding: "10px", marginTop: "20px", color: "red"
         }}>
-            <p>&copy; 2025 - Mi Tienda </p>
-            <li><Link to="/acercade" style={{
-                color: "red",
-                textDecoration: "none"
-            }}>Acerca de Mi Tienda</Link></li>
-            <li><Link to="/contacto" style={{
-                color: "red",
-                textDecoration: "none"
-            }}>Contacto</Link></li>
-
+            <ul style={{
+                listStyle: "none", display: "flex", justifyContent: "space-around", margin: 0
+            }}>
+                <p>&copy; 2025 - Mi Tienda </p>
+                <li><Link to="/acercade" style={{
+                    color: "red",
+                    textDecoration: "none"
+                }}>Acerca de Mi Tienda</Link></li>
+                <li><Link to="/contacto" style={{
+                    color: "red",
+                    textDecoration: "none"
+                }}>Contacto</Link></li>
+            </ul>
         </footer>
     );
 } export default Footer;
